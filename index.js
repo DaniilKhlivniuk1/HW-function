470a7d0cdcd67373bc651e46b908379566e1cf96
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2024-09-21 12:27:01 +0300

    change

---
+++ index.js
@@ -0,0 +1,78 @@
+// 1
+const logItems = function (numb) {
+    let j;
+    for (let i = 0; i <= numb.length - 1; i++) {
+      j = i + 1;
+      console.log(j + " - " + numb[i]);
+    }
+    return;
+  };
+  logItems(["Mango", "Poly", "Ajax"]);
+//   2
+  const calculateEngravingPrice = function (message, pricePerWord) {
+    const words = message.split(" ");
+    const price = pricePerWord * words.length;
+    return price;
+  };
+  console.log(calculateEngravingPrice("Hello it's nice to meet you!", 100));
+//   3
+  const findLongestWord = function (letters) {
+    const words = letters.split(" ");
+    let lonegstWord;
+    let numberOfLetters = 0;
+    for (let word of words) {
+      if (word.length > numberOfLetters) {
+        lonegstWord = word;
+        numberOfLetters = word.length;
+      }
+    }
+    return lonegstWord;
+  };
+  console.log(findLongestWord("Hello it's nice to meet you!"));
+//   4
+  const formatString = function (string) {
+    let result = string;
+    if (string.length > 40) {
+      result = string.slice(0, 40) + "...";
+    }
+    return result;
+  };
+  console.log(formatString("Тут менше 40 символів"));
+  console.log(formatString("Тут юільше 40 символів"));
+//   5
+  const checkForSpam = function (message) {
+    let result;
+    if (
+      message.toLowerCase().includes("spam") === true ||
+      message.toLowerCase().includes("sale") === true
+    ) {
+      result = true;
+    } else {
+      result = false;
+    }
+    return result;
+  };
+  console.log(checkForSpam("SaLe"));
+  console.log(checkForSpam("SpAm"));
+  console.log(checkForSpam("No S-word"));
+//   6
+  let input;
+  const num = [];
+  let total = 0;
+  for (let i = 0; i <= 1000; i++) {
+    input = prompt("Введіть число:");
+    if (input === null) {
+      break;
+    }
+    input = Number.parseInt(input);
+    if (isNaN(input)) {
+      alert("Було введено не число, попробуйте ще раз");
+      continue;
+    }
+    num.push(input);
+    console.log(num);
+  }
+  for (const number of num) {
+    total += number;
+  }
+  console.log(`Сумма введених чисел: ${total}`);
